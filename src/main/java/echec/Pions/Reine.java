733e4dc966387345ab001021632aa7062e6c1444
Ronan Garrigues <ronan.garrigues@etu.univ-amu.fr>
2024-06-03 15:27:42 +0200

    Amélioration class pions

--- src/main/java/echec/Pions/Reine.java
+++ src/main/java/echec/Pions/Reine.java
@@ -2,12 +2,21 @@ package echec.Pions;
 
 public class Reine extends Pions{
 
-    public Reine(String couleur){
-        super(couleur, posY, posX, true);
+    public Reine(String couleur, String posX, int posY) {
+        super(couleur, posX, posY);
     }
 
-    public void deplacement(){
+    @Override
+    public void deplacement(String posXDep, int posYDep, String posXFin, int posYFin){
 
+    }
+
+    @Override
+    public boolean peutDeplacer(int posYDep, String posXDep, int posYFin, String posXFin) {
+        int startX = posXDep.charAt(0) - 'a' + 1;
+        int endX = posXFin.charAt(0) - 'a' + 1;
+
+        return (Math.abs(startX - endX) == Math.abs(posYDep - posYFin) || (startX == endX || posYDep == posYFin));
     }
 
 }
--- src/main/java/echec/Pions/Tour.java
+++ src/main/java/echec/Pions/Tour.java
@@ -2,12 +2,18 @@ package echec.Pions;
 
 public class Tour extends Pions{
 
-    public Tour(String couleur){
-        super(couleur, posY, posX, true);
+    public Tour(String couleur, String posX, int posY) {
+        super(couleur, posX, posY);
+    }
+    public void deplacement(String posXDep, int posYDep, String posXFin, int posYFin){
+
     }
 
-    public void deplacement(){
+    public boolean peutDeplacer(int posYDep, String posXDep, int posYFin, String posXFin) {
+        int startX = posXDep.charAt(0) - 'a' + 1;
+        int endX = posXFin.charAt(0) - 'a' + 1;
 
+        return (startX == endX || posXDep == posXFin);
     }
 
 }
