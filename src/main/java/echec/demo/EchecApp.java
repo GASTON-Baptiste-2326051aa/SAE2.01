43b92e1c3d682d0f2a58e7834bfd5e923a16aee8
Goncalves Rodrigues Alex <alex.GONCALVES-RODRIGUES@etu.univ-amu.fr>
2024-06-03 15:46:37 +0200

    Mise en place du fond de l'application en gris

--- src/main/java/echec/demo/EchecApp.java
+++ src/main/java/echec/demo/EchecApp.java
@@ -15,6 +15,7 @@ public class EchecApp extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(EchecApp.class.getResource("view/accueil.fxml"));
         scene = new Scene(loader.load());
+        scene.getRoot().setStyle("-fx-background-color: rgb(48, 46, 43);");
         stage.setTitle("Echec");
         stage.setScene(scene);
         stage.show();
