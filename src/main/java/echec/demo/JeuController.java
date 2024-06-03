1e3386cda3c91fa3e8bdaeb65d3874f974e8d6bb
Manon VERHILLE <manon.verhille@etu.univ-amu.fr>
2024-06-03 15:24:44 +0200

    Classe JeuController -> fonctions gérant le timer faites

--- src/main/java/echec/demo/JeuController.java
+++ src/main/java/echec/demo/JeuController.java
@@ -1,11 +1,15 @@
 package echec.demo;
 
 import javafx.fxml.FXML;
+
+import echec.Pions.*;
+
 import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
 import javafx.scene.layout.GridPane;
-
-import javax.swing.text.PasswordView;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.swing.*;
 import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,12 +17,21 @@ import java.util.ResourceBundle;
 public class JeuController implements Initializable {
 
     @FXML
-    private Label timer;
+    private Label labelTimerJ1;
+    @FXML
+    private Label labelTimerJ2;
+
+    private Timer timerJ1 = new Timer();
+    private Timer timerJ2 = new Timer();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Initialisation de la grille");
+
+
         timerJoueur1On();
+
+
     } // Marche avec le bouton JOUER
 
     public void deplacementJoueur1(){
@@ -33,32 +46,56 @@ public class JeuController implements Initializable {
         timerJoueur1On();
     }
 
-    public void comparePionsJ1toJ2(){
-        if(pion1.getPosX == pion2.getPosX && pion1.getPosY == pion2.getPosY && (!(pion1.getCouleur().equals(pion2.getCouleur()){
-            //Si pion 2 est un roi : fin de la partie.
-            pion2.setEtat == false;
+    public void comparePions(Pions pion1, Pions pion2){
+        if(pion1.getPosX() == pion2.getPosX() && pion1.getPosY() == pion2.getPosY() && !(pion1.getCouleur().equals(pion2.getCouleur()))){
+            pion2.setEtat(false);
         }
     }
 
-    public void comparePionsJ2toJ1(){
-        if(pion1.getPosX == pion2.getPosX && pion1.getPosY == pion2.getPosY && !(pion1.getCouleur().equals(pion2.getCouleur())){
-            //Si pion 1 est un roi : fin de la partie.
-            pion1.setEtat == false;
-        }
+    public void situationRoi(Roi roi){
+        //TODO
     }
 
     public void timerJoueur1On(){
-        // TODO
+        timerJ2.scheduleAtFixedRate(new TimerTask() {
+            int i = 300;
+            @Override
+            public void run() {
+                labelTimerJ2.setText(String.valueOf(i) + "secondes");
+                i--;
+
+                if (i < 0) {
+                    timerJ2.cancel();
+                    labelTimerJ2.setText("Time Over");
+                }
+            }
+        }, 0, 100);
+
+        timerJoueur2Off();
     }
 
     public void timerJoueur2On(){
-        // TODO
+        timerJ2.scheduleAtFixedRate(new TimerTask() {
+            int i = 300;
+            @Override
+            public void run() {
+                labelTimerJ1.setText(String.valueOf(i) + "secondes");
+                i--;
+
+                if (i < 0) {
+                    timerJ1.cancel();
+                    labelTimerJ1.setText("Time Over");
+                }
+            }
+        }, 0, 100);
+
+        timerJoueur1Off();
     }
     public void timerJoueur1Off(){
-        // TODO
+        timerJ1.cancel();
     }
     public void timerJoueur2Off(){
-        // TODO
+        timerJ2.cancel();
     }
 
 }
