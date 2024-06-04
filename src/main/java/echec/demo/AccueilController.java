ce00609f8f69542db0eeb22728e5ded1693c1fb3
Goncalves Rodrigues Alex <alex.GONCALVES-RODRIGUES@etu.univ-amu.fr>
2024-06-04 13:48:25 +0200

    Mise a jour des bouttons + reglages d'un carré sur le plateau

--- src/main/java/echec/demo/AccueilController.java
+++ src/main/java/echec/demo/AccueilController.java
@@ -1,19 +1,32 @@
 package echec.demo;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class AccueilController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AccueilController implements Initializable {
+
 
     @FXML
     private BorderPane borderPane;
 
     @FXML
-    private Button button1;
+    private Button bouton1;
     @FXML
-    private Button button2;
+    private Button bouton2;
 
+    private ButtonController buttonController;
 
-
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        buttonController = new ButtonController();
+        buttonController.initButton(bouton1, bouton2);
+    }
 }
