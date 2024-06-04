04e1211299bd052ee2074b10f63728fb95e11d94
g23006766 <baptiste.gaston@etu-univ.amu.fr>
2024-06-04 09:32:31 +0200

    visuel bouttons accueil

--- src/main/java/echec/demo/AccueilController.java
+++ src/main/java/echec/demo/AccueilController.java
@@ -1,12 +1,19 @@
 package echec.demo;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
 public class AccueilController {
 
     @FXML
     private BorderPane borderPane;
+
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+
 
 
 }
