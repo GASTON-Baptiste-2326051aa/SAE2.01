5e25ff8eaceba17cba81bdb8fed370b37992a4d1
Goncalves Rodrigues Alex <alex.GONCALVES-RODRIGUES@etu.univ-amu.fr>
2024-06-05 09:49:13 +0200

    Fin de la mise en place du compteur

--- src/main/java/echec/demo/UnVController.java
+++ src/main/java/echec/demo/UnVController.java
@@ -6,9 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -27,15 +25,26 @@ public class UnVController implements Initializable {
     private Label timerLabel;
     @FXML
     private Label timerLabel2;
+    @FXML
+    private Button startButton;
+    @FXML
+    private Button pauseButton;
 
     private ButtonController buttonController;
 
     private Timer timer1;
     private Timer timer2;
 
+    private TimerTask timerTask1;
+    private TimerTask timerTask2;
+
     private int tempsRestant1;
     private int tempsRestant2;
 
+    private boolean isTimer1Running = false;
+    private boolean isTimer2Running = false;
+
+    @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         buttonController = new ButtonController();
         buttonController.initButton3(bouton3);
@@ -44,47 +53,84 @@ public class UnVController implements Initializable {
         timerBox.getSelectionModel().selectFirst();
 
         timerBox.valueProperty().addListener((observable, oldValue, newValue) -> {
-            String timeText = newValue + "Minute";
+            String timeText = newValue + " Minutes";
             timerLabel.setText(timeText);
             timerLabel2.setText(timeText);
         });
 
-        Button startButton = new Button("Jouer");
-        startButton.setOnAction(e -> startTimers());
+        startButton.setOnAction(e -> startTimer1());
+        pauseButton.setOnAction(e -> toggleTimers());
     }
 
-    private void startTimers() {
-        int tempsSelectionne = timerBox.getValue() * 60;
+    private void startTimer1() {
+        Integer selectedValue = timerBox.getValue();
+        if (selectedValue == null) {
+            throw new IllegalStateException("Timer value is not selected.");
+        }
 
+        int tempsSelectionne = selectedValue * 60;
         tempsRestant1 = tempsSelectionne;
         tempsRestant2 = tempsSelectionne;
 
         timer1 = new Timer();
-        timer2 = new Timer();
+        timerTask1 = createTimerTask(timerLabel, () -> --tempsRestant1);
 
-        timer2.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                Platform.runLater(() -> updateTimer(timerLabel, --tempsRestant1));
-            }
-        }, 1000, 1000);
-
-        timer1.scheduleAtFixedRate(new TimerTask(){
-            @Override
-            public void run() {
-                Platform.runLater(() -> updateTimer(timerLabel, --tempsRestant2));
-            }
-        }, 1000, 1000);
+        timer1.scheduleAtFixedRate(timerTask1, 1000, 1000);
+        isTimer1Running = true;
     }
 
-    private void updateTimer(Label timerLabel, int tempsRestant1) {
-        if (tempsRestant1 <= 0) {
-            timerLabel.setText("");
+    private void toggleTimers() {
+        if (isTimer1Running) {
             timer1.cancel();
+            isTimer1Running = false;
+            startTimer2();
+        } else if (isTimer2Running) {
             timer2.cancel();
-        }else {
-            int minutes = tempsRestant1 / 60;
-            int seconds = tempsRestant1 % 60;
+            isTimer2Running = false;
+            resumeTimer1();
+        }
+    }
+
+    private void startTimer2() {
+        timer2 = new Timer();
+        timerTask2 = createTimerTask(timerLabel2, () -> --tempsRestant2);
+
+        timer2.scheduleAtFixedRate(timerTask2, 1000, 1000);
+        isTimer2Running = true;
+    }
+
+    private void resumeTimer1() {
+        timer1 = new Timer();
+        timerTask1 = createTimerTask(timerLabel, () -> --tempsRestant1);
+
+        timer1.scheduleAtFixedRate(timerTask1, 1000, 1000);
+        isTimer1Running = true;
+    }
+
+    private TimerTask createTimerTask(Label timerLabel, Runnable updateRemainingTime) {
+        return new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(() -> {
+                    updateRemainingTime.run();
+                    updateTimer(timerLabel);
+                });
+            }
+        };
+    }
+
+    private void updateTimer(Label timerLabel) {
+        int tempsRestant = (timerLabel == this.timerLabel) ? tempsRestant1 : tempsRestant2;
+        if (tempsRestant <= 0) {
+            timerLabel.setText("00:00");
+            if (timerLabel == this.timerLabel && timer1 != null) {
+                timer1.cancel();
+            } else if (timerLabel == this.timerLabel2 && timer2 != null) {
+                timer2.cancel();
+            }
+        } else {
+            int minutes = tempsRestant / 60;
+            int seconds = tempsRestant % 60;
             timerLabel.setText(String.format("%02d:%02d", minutes, seconds));
         }
     }
