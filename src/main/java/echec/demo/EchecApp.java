b3e3fa298c27753a5e9058aba57e9e7bd018c3af
g23006766 <baptiste.gaston@etu-univ.amu.fr>
2024-06-05 09:51:18 +0200

    fenetre popup login
    Correctif

--- src/main/java/echec/demo/EchecApp.java
+++ src/main/java/echec/demo/EchecApp.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -18,6 +19,7 @@ public class EchecApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Screen screen = Screen.getPrimary();
         FXMLLoader loader = new FXMLLoader(EchecApp.class.getResource("view/accueil.fxml"));
         scene = new Scene(loader.load());
         scene.getRoot().setStyle("-fx-background-color: rgb(48, 46, 43);");
@@ -25,6 +27,7 @@ public class EchecApp extends Application {
         stage.setMinWidth(1920);
         stage.setTitle("Echec");
         stage.setScene(scene);
+
         stage.show();
     }
 }
